74604cc6964fafc1c1903ddea9521720cbdc84e3
Benjamin Boateng <benkissa10@gmail.com>
2022-04-22 04:13:52 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class WidgetExample(BoxLayout):
+
+
+    pass
+
+
+class MyloveApp(App):
+    pass
+
+
+class MyGrid(Widget):
+    pass
+
+
+MyloveApp().run()
